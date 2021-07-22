16155bc6ff7404cb3701f5363b881286648e266f
William Kimball <wkkimball043@gmail.com>
2021-07-21 21:36:28 -0500

    dropped working on styles and focusing on navigation and state

--- src/Components/FireScreen/FireScreen.js
+++ src/Components/FireScreen/FireScreen.js
@@ -2,52 +2,66 @@
 // React Native Global Scope Variables
 // https://aboutreact.com/react-native-global-scope-variables/
 
-import React from 'react';
+import React, {Component} from 'react';
 import {Button, View, Text, SafeAreaView} from 'react-native';
 import Slider from '@react-native-community/slider';
-import styles from './FireScreen.scss';
+// import styles from './FireScreen.scss';
+// import Homescreen from '../../../App';
+// import {NavigationContainer} from '@react-navigation/native';
+// import {createStackNavigator} from '@react-navigation/stack';
 
-const FireScreen = ({navigation}) => {
-  global.MyVar = 'https://aboutreact.com';
-
-  return (
-    <SafeAreaView style={styles.flex1}>
-      <View style={styles.view}>
-        <View style={styles.viewCenter}>
-          <Text style={styles.textTop}>This is the First Page of the App</Text>
+// const FireScreen = ({navigation}) => {
+export default class FireScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {ageSlider: 0};
+  }
+  getVal(val) {
+    console.warn(val);
+  }
+  render() {
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <View style={{flex: 1, padding: 16}}>
+          <View
+            style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Text
+              style={{
+                fontSize: 25,
+                textAlign: 'center',
+                marginBottom: 16,
+              }}>
+              Age: {this.state.ageSlider}
+            </Text>
+            <Slider
+              style={{width: 200, height: 40}}
+              minimumValue={0}
+              maximumValue={100}
+              minimumTrackTintColor="#FFFFFF"
+              maximumTrackTintColor="#000000"
+              value={this.ageSlider}
+              onValueChange={val => this.setState({ageSlider: val})}
+              step={1}
+            />
+            <Button
+              onPress={() => this.props.navigation.navigate('Home')}
+              title="Go to Home Page"
+            />
+          </View>
           <Text
             style={{
               fontSize: 18,
               textAlign: 'center',
-              marginBottom: 16,
-              color: 'red',
+              color: 'grey',
             }}>
-            Value of Global Variable is: {global.MyVar}
+            React Native Global Scope Variables
           </Text>
-          <Button
-            onPress={() => navigation.navigate('SecondPage')}
-            title="Go to Second Page"
-          />
         </View>
-        <Text
-          style={{
-            fontSize: 18,
-            textAlign: 'center',
-            color: 'grey',
-          }}>
-          React Native Global Scope Variables
-        </Text>
-        <Text
-          style={{
-            fontSize: 16,
-            textAlign: 'center',
-            color: 'grey',
-          }}>
-          www.aboutreact.com
-        </Text>
-      </View>
-    </SafeAreaView>
-  );
-};
-
-export default FireScreen;
+      </SafeAreaView>
+    );
+  }
+}
